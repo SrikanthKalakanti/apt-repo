0be3506f92d17013f99c0afe6a245ec34f6798e9
Sampath Kumar <sampath@1stsolve.com>
2018-01-11 22:56:10 +0530

    Modified

--- src/main/webapp/MyApp/app/scripts/services/registerService.js
+++ src/main/webapp/MyApp/app/scripts/services/registerService.js
@@ -1,5 +1,5 @@
 angular.module('myAppApp').service('RegisterService', function($http){
-    this.login = function (registerObj) {
+    this.register = function (registerObj) {
         var urlBase = 'http://localhost:8080';
         return $http.post(urlBase+'/apt/customer/register', registerObj);
     }
