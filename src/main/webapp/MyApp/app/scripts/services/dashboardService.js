04d6ca48f846f2f94831e3871b2ccc05361cbc54
Sampath Kumar <sampath@1stsolve.com>
2018-01-21 15:47:12 +0530

    Fixed the things

--- src/main/webapp/MyApp/app/scripts/services/dashboardService.js
+++ src/main/webapp/MyApp/app/scripts/services/dashboardService.js
@@ -2,6 +2,6 @@ angular.module('myAppApp').service('DashboadService', function($http){
     this.getAllClients = function (customerId) {
         console.log(customerId);
         var urlBase = 'http://localhost:8080';
-        return $http.get(urlBase+'client/getallclientsbycustomerid?customerId='+customerId);
+        return $http.get(urlBase+'/apt/client/getallclientsbycustomerid?customerId='+customerId);
     }
 });
