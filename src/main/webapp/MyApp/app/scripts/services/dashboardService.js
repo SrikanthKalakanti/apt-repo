4223bf06d1284881f8a46bde4aac1b05b0602631
Sampath Kumar <sampath@1stsolve.com>
2018-01-21 15:53:20 +0530

    Fixed

--- src/main/webapp/MyApp/app/scripts/services/dashboardService.js
+++ src/main/webapp/MyApp/app/scripts/services/dashboardService.js
@@ -1,7 +1,8 @@
 angular.module('myAppApp').service('DashboadService', function($http){
     this.getAllClients = function (customerId) {
         console.log(customerId);
+        var cusomerObj = {"customerId" : customerId};
         var urlBase = 'http://localhost:8080';
-        return $http.get(urlBase+'/apt/client/getallclientsbycustomerid?customerId='+customerId);
+        return $http.get(urlBase+'/apt/client/getallclientsbycustomerid?customerId='+cusomerObj);
     }
 });
