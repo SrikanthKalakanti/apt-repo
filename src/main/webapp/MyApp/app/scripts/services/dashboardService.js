b6c0973c939e0b94300d907aeac599292c3f7666
Sampath Kumar <sampath@1stsolve.com>
2018-01-21 15:42:21 +0530

    Fixed the changes

--- src/main/webapp/MyApp/app/scripts/services/dashboardService.js
+++ src/main/webapp/MyApp/app/scripts/services/dashboardService.js
@@ -2,6 +2,6 @@ angular.module('myAppApp').service('DashboadService', function($http){
     this.getAllClients = function (customerId) {
         console.log(customerId);
         var urlBase = 'http://localhost:8080';
-        return $http.get(urlBase+'client/getallclientsbycustomerid?customerId='+customerId,obj);
+        return $http.get(urlBase+'client/getallclientsbycustomerid?customerId='+customerId);
     }
 });
