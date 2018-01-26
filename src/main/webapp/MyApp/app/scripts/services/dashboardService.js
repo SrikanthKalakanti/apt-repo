e26110dda55b25e7fa01706de76819664a7fb056
Sampath Kumar <sampath@1stsolve.com>
2018-01-26 21:41:36 +0530

    Fixed the issues

--- src/main/webapp/MyApp/app/scripts/services/dashboardService.js
+++ src/main/webapp/MyApp/app/scripts/services/dashboardService.js
@@ -4,14 +4,14 @@ angular.module('myAppApp').service('DashboadService', function($http,$sce){
         var cusomerObj = {customerId : customerId};
         var urlBase = 'http://localhost:8080';
         return $http({
-            url : $sce.trustAsUrl(urlBase+'/apt/client/getallclientsbycustomerids?customerId='+customerId),
+            url : $sce.trustAsUrl(urlBase+'/apt/client/getallclientsbycustomerid?customerId='+customerId),
             method: 'GET',
             headers:{
-            'Access-Control-Allow-Origin': 'http://localhost:9000',
+            'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type, X-Requested-With',
-            "Content-Type": "application/json; charset=utf8",
-            'X-Random-Shit':'123123123'
+            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+            'Access-Control-Allow-Credentials' : true,
+            "Content-Type": "application/json"
             }
         });
         //return $http.get(urlBase+'/apt/client/getlistofclientdetails/'+customerId);
