417788d4c331ccc74ef847577602271060f9fc17
Gambheerrao <AF66949@ad.wellpoint.com>
2018-05-21 19:06:49 +0530

    Completed the basic info with the API integration

--- src/main/webapp/apt-ui/src/app/components/customer-details/basic-info/basic-info.component.ts
+++ src/main/webapp/apt-ui/src/app/components/customer-details/basic-info/basic-info.component.ts
@@ -12,10 +12,12 @@ import { BASICINFO } from "../../../mocks/basicInfo";
 export class BasicInfoComponent implements OnInit {
   isDataAvailable = false;
   isEdit = false;
+  loading = false;
   errors: Errors = new Errors();
   basicData;
 
   model: any = {
+    nameOfTheBussiness: "",
     status: "-1",
     state: "-1",
     customerId: localStorage.getItem("customerId")
@@ -24,40 +26,88 @@ export class BasicInfoComponent implements OnInit {
   constructor(
     private customerDetailsService: CustomerDetailsService,
     private errorService: ErrorService
-  ) {}
+  ) {
+    if (!this.basicData) {
+      this.basicData = {};
+    }
+    if (!this.model) {
+      this.model = {};
+    }
+  }
 
   ngOnInit() {
     this.getBasicInput();
   }
 
   showForm() {
-    if(!this.isEdit){
+    // debugger;
+    if (!this.isEdit) {
       this.isEdit = true;
       this.model = this.basicData;
-    } else{
+    } else {
       this.isEdit = false;
     }
   }
 
+  cancel() {
+    this.isEdit = false;
+  }
+
   getBasicInput() {
     this.isDataAvailable = true;
-    this.basicData = BASICINFO;
-    console.log(this.basicData);
-    // this.customerDetailsService.getBasicInput().subscribe(
-    //   data => {
-    //     if (data.result) {
-    //       this.basicData = data.result;
-    //       this.isDataAvailable = true;
-    //     } else {
-    //       this.isDataAvailable = false;
-    //     }
-    //     //this.dtTrigger.next();
-    //   },
-    //   err => {
-    //     this.isDataAvailable = false;
-    //     this.errors = err.error;
-    //     this.errorService.error(this.errors);
-    //   }
-    // );
+    // this.basicData = BASICINFO;
+    // console.log(this.basicData);
+    this.customerDetailsService.getBasicInput().subscribe(
+      data => {
+        if (data.result) {
+          this.basicData = data.result;
+          this.isDataAvailable = true;
+        } else {
+          this.isDataAvailable = false;
+        }
+      },
+      err => {
+        this.isDataAvailable = false;
+        this.errors = err.error;
+        this.errorService.error(this.errors);
+      }
+    );
+  }
+
+  updateClient() {
+    this.loading = true;
+    this.errors = new Errors();
+    const formValues = this.model;
+    // console.log(formValues);
+    // this.isEdit = false;
+    // this.loading = false;
+    this.customerDetailsService.updateBasicInput().subscribe(
+      data => {
+        this.errorService.success(JSON.parse(data));
+        this.isEdit = false;
+        this.loading = false;
+      },
+      err => {
+        this.isDataAvailable = false;
+        this.isEdit = false;
+        this.errors = err.error;
+        this.errorService.error(this.errors);
+      }
+    );
+  }
+  addBasicInfo() {
+    this.customerDetailsService.addBasicInfo().subscribe(
+      data => {
+        this.errorService.success(JSON.parse(data));
+        this.isEdit = false;
+        this.loading = false;
+      },
+      err => {
+        this.isDataAvailable = false;
+        this.isEdit = false;
+        this.errors = err.error;
+        this.errorService.error(this.errors);
+      }
+    );
   }
 }
--- src/main/webapp/apt-ui/src/app/services/customer-details.service.ts
+++ src/main/webapp/apt-ui/src/app/services/customer-details.service.ts
@@ -1,25 +1,47 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { ApiService } from './api.service';
-import { AuthService } from './auth.service';
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+import { ApiService } from "./api.service";
+import { AuthService } from "./auth.service";
 
 @Injectable()
 export class CustomerDetailsService {
-
   constructor(
     private apiService: ApiService,
     private authService: AuthService
-  ) { }
+  ) {}
 
   getBasicInput(): Observable<any> {
-    const path = '/apt/client/basicinput';// + '?customerId=' + localStorage.getItem('customerId');
-    const data = JSON.parse(localStorage.getItem('clientData'));
+    const path = "/apt/client/basicinput"; // + '?customerId=' + localStorage.getItem('customerId');
+    const data = JSON.parse(localStorage.getItem("clientData"));
     console.log(data.clientId);
-    const obj = {clientId: data.clientId};
-    return this.apiService.post(path, obj, this.authService.getHeaders())
+    const obj = { clientId: data.clientId };
+    return this.apiService
+      .post(path, obj, this.authService.getHeaders())
       .map(data => {
         return data;
       });
   }
 
+  updateBasicInput(): Observable<any> {
+    const path = "/apt/client/basicinput/update"; // + '?customerId=' + localStorage.getItem('customerId');
+    const data = JSON.parse(localStorage.getItem("clientData"));
+    console.log(data.clientId);
+    const obj = { clientId: data.clientId };
+    return this.apiService
+      .post(path, obj, this.authService.getHeaders())
+      .map(data => {
+        return data;
+      });
+  }
+  addBasicInfo(): Observable<any> {
+    const path = "/apt/client/basicinput/create"; // + '?customerId=' + localStorage.getItem('customerId');
+    const data = JSON.parse(localStorage.getItem("clientData"));
+    console.log(data.clientId);
+    const obj = { clientId: data.clientId };
+    return this.apiService
+      .post(path, obj, this.authService.getHeaders())
+      .map(data => {
+        return data;
+      });
+  }
 }
